4433e8dc74b73df23b4aa64b36eb29835786c1a3
Daviduo <davide0400@gmail.com>
2023-04-04 15:06:06 +0200

    edited comment again

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -33,12 +33,12 @@ Console.WriteLine("Cibo preferito ma meno: " + CiboprefeMaMeno.ToString());
 if (lunghezzaArray % 2 == 0)
 {
     var mid = lunghezzaArray / 2;
-    Console.WriteLine("I cibi sono pari, dunque il cibo preferito di mezzo: " + cibiPrefe[mid]);
+    Console.WriteLine("I cibi sono pari, dunque il cibo preferito di mezzo è: " + cibiPrefe[mid]);
 }
 //Se l'array è dispari allora prendi i due elementi di mezzo e li metti in un array
 else
 {
     var mid = lunghezzaArray / 2;
-    Console.WriteLine("I cibi sono dispari, dunque cibi preferiti di mezzo: " + cibiPrefe[mid - 1] + " e " + cibiPrefe[mid]);
+    Console.WriteLine("I cibi sono dispari, dunque i cibi preferiti di mezzo sono: " + cibiPrefe[mid - 1] + " e " + cibiPrefe[mid]);
 }
 
